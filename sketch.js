b697fba2c4116893d2ffa1e1e41534ba7e2bb4a0
larissafrts <larissa.costa.silva19@escola.pr.gov.br>
2025-06-13 09:06:40 -0300

    cartoes_13-06-2025

---
+++ sketch.js
@@ -0,0 +1,11 @@
+criaCartao(
+'Programacao',
+'O que e JavaScript?',
+'JavaScript e uma linguagem de programacao.'
+);
+criaCartao(
+'Geografia',
+'Qual a capital da Franca?',
+'A capital da Franca e Paris.'
+);
+criaCartao (  
